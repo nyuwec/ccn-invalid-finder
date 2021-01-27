291d7b48c27ec3141e716f2ee1003ba8b1f345a8
Mihály Köles <mihaly.koles@prezi.com>
2021-01-27 18:33:23 +0100

    filtering out files with ext: csv

--- src/finder.ts
+++ src/finder.ts
@@ -1,21 +1,29 @@
 import * as fs from 'fs'
+import * as path from 'path'
 import * as parse from 'csv-parse/lib/sync'
 
 export default function listInvalidFiles(dirname: string): {invalidFiles: string[], allFiles: string[]} {
-  let allFiles = fs.readdirSync(dirname).sort()
+  let allFiles: fs.Dirent[] = fs.readdirSync(dirname, {
+    encoding: 'utf8',
+    withFileTypes: true
+  }).sort()
   let invalidFiles = allFiles//.slice(0, 20)
-    .filter((filename) => {
-      let path = dirname + '/' + filename
-      let data = fs.readFileSync(path, 'utf8')
-      let records = <Array<Array<string>>>parse(data, {fromLine: 7})
-      let invalidSampleFlow = records.some(el => parseFloat(el[17]) < 40.0)
-      let invalidAlarmCode = records.some(el => parseFloat(el[47]) > 0.0)
+    .filter((dirEntity) => {
+      if (dirEntity.isFile() && path.extname(dirEntity.name) == '.csv') {
+        let absPath = dirname + '/' + dirEntity.name
+        let data = fs.readFileSync(absPath, 'utf8')
+        let records = <Array<Array<string>>>parse(data, {fromLine: 7})
+        let invalidSampleFlow = records.some(el => parseFloat(el[17]) < 40.0)
+        let invalidAlarmCode = records.some(el => parseFloat(el[47]) > 0.0)
 
-      return (invalidSampleFlow || invalidAlarmCode)
+        return (invalidSampleFlow || invalidAlarmCode)
+      } else {
+        return false
+      }
     })
 
   return {
-    invalidFiles: invalidFiles,
-    allFiles: allFiles
+    invalidFiles: invalidFiles.map((_) => _.name),
+    allFiles: allFiles.map((_) => _.name)
   }
 }
