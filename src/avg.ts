dd27e057d0c2f38cf3d140d1f824a4e3551846c2
Mihály Köles <mihaly.koles@prezi.com>
2021-02-10 22:45:28 +0100

    grouped averages

--- src/avg.ts
+++ src/avg.ts
@@ -10,31 +10,37 @@ const Cols = {
 }
 const START_ROW = 5
 
-type RowType = {date: Excel.CellValue, vals: Excel.CellValue[]}
+type RowType = {date: string, vals: number[]}
 
 workbook.xlsx.readFile('data/2.xlsx')
-    .then((wb) => {
-      let worksheet = wb.getWorksheet(1);
-      let firstDateCell = getRowData(worksheet, START_ROW).date
-      let current10MinGroup = get10MinTimeGroupKey(firstDateCell as string)
+  .then((wb) => {
+    let worksheet = wb.getWorksheet(1);
 
-      let groupedRows = new MMap<string, Array<RowType>>()
-      for (let i=START_ROW;i<=worksheet.actualRowCount;i++) {
-        let row = getRowData(worksheet, i)
-        let row10MinGroup = get10MinTimeGroupKey(row.date as string)
-        let arr = groupedRows.getOrElse(row10MinGroup, new Array<RowType>())
-        arr.push(row)
-        groupedRows.set(row10MinGroup, arr)
+    let groupedRows = new MMap<string, Array<RowType>>()
+    for (let i=START_ROW;i<=worksheet.actualRowCount;i++) {
+      let row = getRowData(worksheet, i)
+      let row10MinGroup = get10MinTimeGroupKey(row.date as string)
+      let arr = groupedRows.getOrElse(row10MinGroup, new Array<RowType>())
+      arr.push(row)
+      groupedRows.set(row10MinGroup, arr)
+    }
+    groupedRows.forEach((groupedRows, index) => {
+      console.log(`"${index}" has ${groupedRows.length} items`)
+      let avgCols = []
+      if (groupedRows.length > 0) {
+        let colNum = groupedRows[0].vals.length
+        for (let col = 0; col < colNum; col ++) {
+          let colVals = groupedRows.map(row => row.vals[col])
+          avgCols[col] = (
+            (colVals.reduce((prev, current) => prev + current, 0) / colVals.length)
+          ).toFixed(4)
+        }
       }
-      groupedRows.forEach((val, index) => {
-        console.log(`"${index}" has ${val.length} items`)
-      })
-
-
-
-        //row.getCell(1).value = 5; // A5's value set to 5
-        //row.commit();
-        //return workbook.xlsx.writeFile('new.xlsx');
+      console.log(`    AVGS: ${avgCols}`)
+    })
+    //row.getCell(1).value = 5; // A5's value set to 5
+    //row.commit();
+    //return workbook.xlsx.writeFile('new.xlsx');
     })
     .catch((err)=> {
       console.log(err)
@@ -43,12 +49,12 @@ workbook.xlsx.readFile('data/2.xlsx')
 function getRowData(worksheet: Excel.Worksheet, rowNum: number): RowType {
   let row = worksheet.getRow(rowNum);
   let date: Excel.CellValue = row.getCell(Cols.DATE).value
-  let vals = []
+  let vals: number[] = []
   for (let i=Cols.DATE+1;i<=Cols.LAST;i++) {
-   vals.push(row.getCell(i).value)
+   vals.push(row.getCell(i).value as number)
   }
   return {
-    date: date,
+    date: date as string,
     vals: vals
   }
 }
