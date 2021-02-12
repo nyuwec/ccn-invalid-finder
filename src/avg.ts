cdd1647705671ac0abe771e4f86c4d079cb98e64
Mihály Köles <mihaly.koles@prezi.com>
2021-02-12 19:41:12 +0100

    writing out avgcols

--- src/avg.ts
+++ src/avg.ts
@@ -2,7 +2,8 @@ import * as Excel from 'exceljs'
 import * as moment from 'moment'
 import { MMap } from './models/MMap'
 
-let workbook = new Excel.Workbook();
+let inXls = new Excel.Workbook();
+let outXls = new Excel.Workbook();
 
 const Cols = {
   "DATE": 1,
@@ -12,35 +13,40 @@ const START_ROW = 5
 
 type RowType = {date: string, vals: number[]}
 
-workbook.xlsx.readFile('data/2.xlsx')
+inXls.xlsx.readFile('data/2.xlsx')
   .then((wb) => {
     let worksheet = wb.getWorksheet(1);
 
     let groupedRows = new MMap<string, Array<RowType>>()
     for (let i=START_ROW;i<=worksheet.actualRowCount;i++) {
       let row = getRowData(worksheet, i)
-      let row10MinGroup = get10MinTimeGroupKey(row.date as string)
-      let arr = groupedRows.getOrElse(row10MinGroup, new Array<RowType>())
+      let row10MinGroupKey = get10MinTimeGroupKey(row.date as string)
+      let arr = groupedRows.getOrElse(row10MinGroupKey, new Array<RowType>())
       arr.push(row)
-      groupedRows.set(row10MinGroup, arr)
+      groupedRows.set(row10MinGroupKey, arr)
     }
-    groupedRows.forEach((groupedRows, index) => {
-      console.log(`"${index}" has ${groupedRows.length} items`)
-      let avgCols = []
+
+    const outSheet = outXls.addWorksheet('Aggregated data');
+    let avgCols = new MMap<string, Array<number>>()
+    groupedRows.forEach((groupedRows, key) => {
       if (groupedRows.length > 0) {
         let colNum = groupedRows[0].vals.length
         for (let col = 0; col < colNum; col ++) {
           let colVals = groupedRows.map(row => row.vals[col])
-          avgCols[col] = (
-            (colVals.reduce((prev, current) => prev + current, 0) / colVals.length)
-          ).toFixed(4)
+          let arr = avgCols.getOrElse(key, new Array<number>())
+          arr[col] = (
+            colVals.reduce(
+              (prev, current) => prev + current,
+              0
+            ) / colVals.length)
+          avgCols.set(key, arr)
         }
       }
-      console.log(`    AVGS: ${avgCols}`)
+      let newRow = outSheet.addRow([moment.utc(key).toDate(), ...avgCols.getOrElse(key, [])])
+      newRow.commit()
     })
-    //row.getCell(1).value = 5; // A5's value set to 5
-    //row.commit();
-    //return workbook.xlsx.writeFile('new.xlsx');
+
+    outXls.xlsx.writeFile('data/new.xlsx')
     })
     .catch((err)=> {
       console.log(err)
@@ -63,6 +69,6 @@ function get10MinTimeGroupKey(timeString: string): string {
   let ts = moment.utc(timeString)
   let min = ts.minute()
   let groupMin = Math.floor(min / 10)
-  let prefix = ts.format("YYYY-MM-DD:HH")
-  return `${prefix}:${groupMin}`
+  let prefix = ts.format("YYYY-MM-DDTHH")
+  return `${prefix}:${groupMin}0:00.000Z`
 }
