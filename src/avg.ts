fc8ca52ed6657efc425865e64affbc2230250fa1
Mihály Köles <mihaly.koles@prezi.com>
2021-02-13 01:52:34 +0100

    refactor1

--- src/avg.ts
+++ src/avg.ts
@@ -6,24 +6,37 @@ let outXls = new Excel.Workbook();
 
 const Cols = {
   "DATE": 1,
-  "LAST": 31
 }
 const START_ROW = 5
 
-type RowType = {date: moment.Moment, vals: number[]}
-class GroupedRows extends MMap<string, Array<RowType>> {}
+type DateRow = {date: moment.Moment, vals: number[]}
+class GroupedRows extends MMap<string, Array<DateRow>> {
+  pushFrom(row: DateRow) {
+    let row10MinGroupKey = this.get10MinTimeGroupKey(row.date)
+    let arr = this.getOrElse(row10MinGroupKey, new Array<DateRow>())
+    arr.push(row)
+    this.set(row10MinGroupKey, arr)
+  }
+
+  private get10MinTimeGroupKey(momentVal: moment.Moment): string {
+    let min = momentVal.minute()
+    let groupMin = Math.floor(min / 10)
+    let prefix = momentVal.format("YYYY-MM-DDTHH")
+    return `${prefix}:${groupMin}0:00.000Z`
+  }
+}
 class AvgResults extends MMap<string, Array<number>> {}
 
-// loadDataFromStream('data/Balatonszabadi_OPC_full.xlsx')
+loadDataFromStream('data/Balatonszabadi_OPC_full.xlsx')
 
-loadDataFromCSV('data/Balatonszabadi_OPC_full.csv')
-  .then((groupedRows) => {
-    const avgResults = calculateAvg(groupedRows)
-    writeAvgs(avgResults)
-  })
-  .catch((err)=> {
-    console.log(err)
-  })
+// loadDataFromCSV('data/Balatonszabadi_OPC_full.csv')
+//   .then((groupedRows) => {
+//     const avgResults = calculateAvg(groupedRows)
+//     writeAvgs(avgResults)
+//   })
+//   .catch((err)=> {
+//     console.log(err)
+//   })
 
 
 // LOADERS
@@ -45,8 +58,9 @@ async function loadDataFromCSV(fileName: string): Promise<GroupedRows> {
 
 async function loadDataFromStream(fileName: string) {
   const options: Partial<Excel.stream.xlsx.WorkbookStreamReaderOptions> = {
-    sharedStrings: 'emit',
-    hyperlinks: 'emit',
+    sharedStrings: 'ignore',
+    hyperlinks: 'ignore',
+    styles: 'ignore',
     worksheets: 'emit',
     entries: 'emit',
   };
@@ -55,11 +69,8 @@ async function loadDataFromStream(fileName: string) {
     let groupedRows: GroupedRows = new GroupedRows()
     for await (const rawRow of worksheetReader) {
       if (rawRow.number >= START_ROW) {
-        let row = getRawRowData(rawRow)
-        let row10MinGroupKey = get10MinTimeGroupKey(row.date)
-        let arr = groupedRows.getOrElse(row10MinGroupKey, new Array<RowType>())
-        arr.push(row)
-        groupedRows.set(row10MinGroupKey, arr)
+        let row = toDateRow(rawRow)
+        groupedRows.pushFrom(row)
       }
     }
     const avgResults = calculateAvg(groupedRows)
@@ -71,14 +82,16 @@ async function loadDataFromStream(fileName: string) {
 
 // AGGREGATOR FUNCTIONS
 function gatherGroupedRows(worksheet: Excel.Worksheet): GroupedRows {
+  function getRowData(worksheet: Excel.Worksheet, rowNum: number): DateRow {
+    let rawRow = worksheet.getRow(rowNum);
+    return toDateRow(rawRow)
+  }
+
   let groupedRows: GroupedRows = new GroupedRows()
   const lastRowNum = worksheet.actualRowCount
   for (let i=START_ROW;i<=lastRowNum;i++) {
     let row = getRowData(worksheet, i)
-    let row10MinGroupKey = get10MinTimeGroupKey(row.date)
-    let arr = groupedRows.getOrElse(row10MinGroupKey, new Array<RowType>())
-    arr.push(row)
-    groupedRows.set(row10MinGroupKey, arr)
+    groupedRows.pushFrom(row)
   }
   return groupedRows
 }
@@ -115,34 +128,24 @@ function calculateAvg(groupedRows: GroupedRows): AvgResults {
 
 
 // EXCEL PARSING UTILITIES
-function getRowData(worksheet: Excel.Worksheet, rowNum: number): RowType {
-  let rawRow = worksheet.getRow(rowNum);
-  return getRawRowData(rawRow)
-}
-
-function getRawRowData(rawRow: Excel.Row): RowType {
+function toDateRow(rawRow: Excel.Row): DateRow {
   const dateCell = rawRow.getCell(Cols.DATE)
-  let date: moment.Moment
-  if (dateCell.type == Excel.ValueType.Number) {
-    date = excelDate2Date(dateCell.value as number)
-  } else {
-    date = moment.utc(dateCell.value as string, "YYYY.MM.DD hh:mm:ss")
-  }
-  let vals: number[] = []
-  for (let i=Cols.DATE+1;i<=Cols.LAST;i++) {
+  const date: moment.Moment = function () {
+    if (dateCell.type == Excel.ValueType.Number) {
+      return excelDate2Date(dateCell.value as number)
+    } else {
+      return moment.utc(dateCell.value as string, "YYYY.MM.DD hh:mm:ss")
+    }
+  }()
+
+  const vals: number[] = Array<number>()
+  for (let i=Cols.DATE+1;i<=rawRow.cellCount;i++) {
    vals.push(rawRow.getCell(i).value as number)
   }
   return {
     date: date,
     vals: vals
   }
-}
-
-function get10MinTimeGroupKey(momentVal: moment.Moment): string {
-  let min = momentVal.minute()
-  let groupMin = Math.floor(min / 10)
-  let prefix = momentVal.format("YYYY-MM-DDTHH")
-  return `${prefix}:${groupMin}0:00.000Z`
 }
 
 function excelDate2Date(excelDate: number): moment.Moment {
